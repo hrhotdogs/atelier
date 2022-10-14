03e294ebfad7432410ad5d79811b16dd5d536763
Roshan Patel <roshanupatel@gmail.com>
2022-10-14 10:15:36 -0700

    fix error, sorry hotdoggers...

--- client/src/components/Related/Related.jsx
+++ client/src/components/Related/Related.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import Axios from 'axios';
 import CurrentInfo from '../Context.jsx';
+import {TOKEN} from '../../../../config.js';
 
 const Related = () => {
-
   const currentState = React.useContext(CurrentInfo);
+
   let [currentProductID, setCurrentProductID] = currentState.currentProductID;
   let [currentStyleID, setCurrentStyleID] = currentState.currentStyleID;
 
   return (
-    <div>Related...{currentState}</div>
+    <div>Related...{currentProductID}</div>
   )
 }
 
