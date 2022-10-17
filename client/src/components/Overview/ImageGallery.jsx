2d6ca1cf220652fe0757e38535c3642bfedacac7
ryan-leigh <ryanleigh25@gmail.com>
2022-10-16 20:21:01 -0700

    Add main image to ImageGallery

--- client/src/components/Overview/ImageGallery.jsx
+++ client/src/components/Overview/ImageGallery.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import ThumbnailCarousel from './ThumbnailCarousel.jsx';
+const {useState, useEffect} = React;
 
 
-const ImageGallery = ({currentImage, currentStyleThumbnails}) => {
+const ImageGallery = ({currentStyle}) => {
 
+  // Set local state
+  let [mainImageUrl, setMainImageUrl] = useState(currentStyle.photos[0].url);
+  console.log(mainImageUrl);
+
+  useEffect(() => {
+    setMainImageUrl(currentStyle.photos[0].url)
+  }, [currentStyle])
 
   return (
-    <div>
-      Image gallery
-      <ThumbnailCarousel currentImage={currentImage} currentStyleThumbnails={currentStyleThumbnails} />
+    <div style={{backgroundImage: `url('${mainImageUrl}')`, backgroundSize: 'cover', width: '550px', height: '550px', backgroundPosition: 'center'}}>
+     <ThumbnailCarousel mainImageUrl={mainImageUrl} setMainImageUrl={setMainImageUrl} />
     </div>
   )
 }
--- client/src/components/Overview/Overview.jsx
+++ client/src/components/Overview/Overview.jsx
@@ -21,7 +21,7 @@ const Overview = () => {
   // Create local state and global vars
   let [productInfo, setProductInfo] = useState({});
   let [styles, setStyles] = useState([]);
-  let [currentStyle, setCurrentStyle] = useState('');
+  let [currentStyle, setCurrentStyle] = useState({photos: [{}]});
   let [ratings, setRatings] = useState({});
 
   // Get info from API when current product ID is updated
@@ -35,6 +35,7 @@ const Overview = () => {
     Axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/products/${currentProductID}/styles`, {headers: {Authorization: TOKEN}})
       .then(response => {
         setCurrentStyleID(response.data.results[0].style_id);
+        console.log(response.data.results[0]);
         setCurrentStyle(response.data.results[0]);
         setStyles(response.data.results.slice());
       })
