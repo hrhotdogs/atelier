4930c00630460244429e5a0c36a2ba02f3418eae
Sean McEntagart <seanmcentagart@Seans-MacBook-Pro.local>
2022-10-17 17:47:16 -0700

    set up review list for current product, npm installed a package for date formatting, set up webpack to handle image files

--- client/src/components/Ratings/Ratings.jsx
+++ client/src/components/Ratings/Ratings.jsx
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import axios from 'axios';
+import { TOKEN } from '../../../../config.js';
+import CurrentInfo from '../Context.jsx';
+import ReviewTile from './ReviewTile.jsx';
 const Ratings = () => {
+  const currentState = useContext(CurrentInfo);
+  const [currentProductID, setCurrentProductID] = currentState.currentProductID;
+  const [listOfReviews, setListOfReviews] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        ` https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/reviews?page&count&sort=relevant&product_id=${currentProductID}`,
+        { headers: { Authorization: `${TOKEN}` } }
+      )
+      .then((results) => {
+        setCurrentProductID(results.data.product);
+        setListOfReviews(results.data.results);
+        console.log(results.data.results);
+      })
+      .catch((error) => console.log(error));
+  }, [currentProductID]);
+
   return (
-    <div>Ratings</div>
-  )
-}
+    <div>
+      {listOfReviews.map((review, index) => (
+        <ReviewTile review={review} key={index} />
+      ))}
+    </div>
+  );
+};
 
 export default Ratings;
