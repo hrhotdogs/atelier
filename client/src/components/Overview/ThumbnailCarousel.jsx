e2b837954a17a3b8cfb240ca054c7a68ac8982ba
ryan-leigh <ryanleigh25@gmail.com>
2022-10-18 13:39:31 -0700

    Add arrow buttons and centering for image carousel

--- client/src/components/Overview/ThumbnailCarousel.jsx
+++ client/src/components/Overview/ThumbnailCarousel.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 
 const ThumbnailCarousel = ({currentStyle, setMainImageUrl}) => {
+
+  const thumbnailPhotos = [];
+  const topOverflowPhotos = [];
+  const bottomOverflowPhotos = [];
+
+
+
   const handleThumbnailClick = (photoUrl) => {
     setMainImageUrl(photoUrl);
   }
   return (
     <div>
       {currentStyle.photos.map((photoObj, index) => (
-        <div style={{backgroundImage: `url('${photoObj.url}')`, backgroundPosition: 'center', backgroundSize: 'cover', width: '75px', height: '75px', margin: '10px'}} key={index} onClick={() => {handleThumbnailClick(photoObj.url)}}></div>
+        <div style={{backgroundImage: `url('${photoObj.url}')`, backgroundPosition: 'center', backgroundSize: 'cover', width: '85px', height: '85px', margin: '10px'}} key={index} onClick={() => {handleThumbnailClick(photoObj.url)}}></div>
       ))}
     </div>
   )
