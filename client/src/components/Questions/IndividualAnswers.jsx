bed7a6017f836627db199fdb1dea00b4c8dcb3b4
Joseph <jredboyz@gmail.com>
2022-10-18 10:05:06 -0700

    Added more info to answers in IndividualAnswers

--- client/src/components/Questions/IndividualAnswers.jsx
+++ client/src/components/Questions/IndividualAnswers.jsx
@@ -2,10 +2,15 @@ import React from 'react';
 import axios from 'axios';
 
 const IndividualAnswers = ({ answer }) => {
+  function onClickHandler() {
+    console.log('clicked')
+  }
+  console.log(format(new Date(), 'mm/dd/yyyy'))
   return (
     <div>
       A: {answer.body}
-      <br />
+      <br/>
+      by {answer.answerer_name}, DATE | Helpful? <span onClick={(e) => onClickHandler()}>Yes</span>
     </div>
   );
 };
