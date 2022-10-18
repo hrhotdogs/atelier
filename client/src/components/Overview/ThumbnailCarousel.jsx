a36a9d83acfc9c5be3ef82ddd1effc2c15117d28
ryan-leigh <ryanleigh25@gmail.com>
2022-10-18 11:33:10 -0700

    Add click handler for thumbnails in image carousel

--- client/src/components/Overview/ThumbnailCarousel.jsx
+++ client/src/components/Overview/ThumbnailCarousel.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
 const ThumbnailCarousel = ({currentStyle, setMainImageUrl}) => {
+  const handleThumbnailClick = (photoUrl) => {
+    setMainImageUrl(photoUrl);
+  }
   return (
     <div>
       {currentStyle.photos.map((photoObj, index) => (
-        <div style={{backgroundImage: `url('${photoObj.url}')`, backgroundPosition: 'center', backgroundSize: 'cover', width: '75px', height: '75px', margin: '10px'}} key={index}></div>
+        <div style={{backgroundImage: `url('${photoObj.url}')`, backgroundPosition: 'center', backgroundSize: 'cover', width: '75px', height: '75px', margin: '10px'}} key={index} onClick={() => {handleThumbnailClick(photoObj.url)}}></div>
       ))}
     </div>
   )
