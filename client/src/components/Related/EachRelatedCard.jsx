d498a7b0136acfb0ed322e27b4ef130a5832cb83
Roshan Patel <roshanupatel@gmail.com>
2022-10-19 19:04:47 -0700

    Spaced out GET requests for related prod info by 1s (total of >6s for 6 related products before all GET requests are placed. Render out prod details on card on completion of requests

--- client/src/components/Related/EachRelatedCard.jsx
+++ client/src/components/Related/EachRelatedCard.jsx
@@ -6,36 +6,44 @@ import { TOKEN } from '../../../../config.js';
 const EachRelatedCard = (props) => {
   const { currentProductID, setCurrentProductID } = React.useContext(ProductIDContext);
 
-  const [product, setProduct] = React.useState({name: "loading", id: "loading"});
+  const [product, setProduct] = React.useState({});
 
+  // AbortController to be used as a signal for the GET and to abort any open requests on unmounting of this component (see the "return" statement in the useEffect fx below)
+  const controller = new AbortController();
 
+  // Promise with async GET for one related product details only resolves after request finishes
   const delayGetProdInfo = (prodID, ms) => new Promise(resolve => setTimeout(() => {
     Axios.get(
       `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/products/${prodID}`,
-      { headers: { Authorization: `${TOKEN}` } }
+      { headers: { Authorization: `${TOKEN}` }, signal: controller.signal }
     )
       .then((res) => {
         resolve(res.data);
-      });
+      })
+      .catch((err) => {})
   }, ms));
 
+  // async fx that awaits the delayGetProdInfo promise to set the product object of this related product to be used in rendering
   const setProdInfo = async (relatedID) => {
     await delayGetProdInfo(relatedID, props.index*1000)
       .then(
         (productObj) => {
-          console.log('productObj', productObj);
-          console.log('index: ', props.index);
           setProduct(productObj);
         }
       )
   }
 
+  // on each change of the global state, GET this related product's details from the API, wait for the request, set this component's product to the requested object
+  // on unmount of this component, abort the current GET if unfinished and setProduct back to an empty object to avoid memory leaks and avoid misrenders of previous render's information
   React.useEffect(() => {
-    console.log("mounting!");
     setProdInfo(props.relatedProduct);
-    return () => console.log('unmounting...');
-  }, [props.currentProductID]);
+    return () => {
+      controller.abort();
+      setProduct({});
+    }
+  }, [currentProductID]);
 
+  // set the global product id to this related product's ID onClick
   const handleRelatedClick = (e) => {
     e.preventDefault();
     setCurrentProductID(product.id);
