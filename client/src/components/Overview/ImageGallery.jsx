84ac5b92c9317bd76120b4418ad0fa2dd41618b8
ryan-leigh <ryanleigh25@gmail.com>
2022-10-21 00:19:39 -0700

    Carousel functionality complete with arrows

--- client/src/components/Overview/ImageGallery.jsx
+++ client/src/components/Overview/ImageGallery.jsx
@@ -6,44 +6,41 @@ const {useState, useEffect} = React;
 const ImageGallery = ({currentStyle}) => {
 
   // Set local state
-  console.log('rendering gallery');
-  console.log('child: ', currentStyle);
   const [mainImageUrl, setMainImageUrl] = useState(currentStyle.photos[0].url);
   const [carouselAtTop, setCarouselAtTop] = useState(true);
-  const [carouselAtBottom, setCarouselAtBottom] = useState(false);
-  console.log(mainImageUrl);
-
-  const handleCarouselScroll = (e) => {
-    setCarouselAtBottom(e.target.scrollHeight - e.target.scrollTop === e.target.clientHeight);
-    setCarouselAtTop(e.target.scrollTop === 0);
-  }
+  const [carouselAtBottom, setCarouselAtBottom] = useState(!(currentStyle.photos.length > 5));
 
   const showTopArrow = () => {
-    if (carouselAtTop) {
-      return  (<button style={{paddingLeft: '22px', paddingRight: '22px'}}>^</button>)
+    if (!carouselAtTop) {
+      return  (<div style={{position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', textAlign: 'center', fontWeight: 'bold'}}><i className="arrow up"></i></div>)
+    } else {
+      return (<div style={{position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', textAlign: 'center'}}></div>)
     }
   }
 
   const showBottomArrow = () => {
-    if (carouselAtBottom) {
-      return  (<button style={{paddingLeft: '22px', paddingRight: '22px'}}>v</button>)
+    if (!carouselAtBottom) {
+      return  (<div style={{position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', textAlign: 'center', fontWeight: 'bold'}}><i className="arrow down"></i></div>)
+    } else {
+      return (<div style={{position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', textAlign: 'center'}}></div>)
     }
   }
 
+  // Set actual default values when API data is received
   useEffect(() => {
       setMainImageUrl(currentStyle.photos[0].url);
+      setCarouselAtTop(true);
+      setCarouselAtBottom(!(currentStyle.photos.length > 5));
   }, [currentStyle])
 
   return (
     <div style={{display: 'flex', flexDirection: 'row'}} data-testid="imageGallery-1">
-      <div style={{display: 'flex', flexDirection:'column', justifyContent: 'center'}}>
-        <div style={{textAlign: 'center'}}>{showTopArrow()}</div>
-        <div className='thumbnailCarousel' style={{overflow: 'auto', maxHeight: '475px'}} onScroll={(e) => {handleCarouselScroll(e)}}>
-          <ThumbnailCarousel currentStyle={currentStyle} setMainImageUrl={setMainImageUrl} />
-        </div>
-        <div style={{textAlign: 'center', marginTop: '10px'}}>{showBottomArrow()}</div>
+      <div style={{display: 'flex', flexDirection:'column', justifyContent: 'center', height: '675px'}}>
+        <div style={{height: '35px', position: 'relative'}}>{showTopArrow()}</div>
+        <ThumbnailCarousel currentStyle={currentStyle} setMainImageUrl={setMainImageUrl} setCarouselAtTop={setCarouselAtTop} setCarouselAtBottom={setCarouselAtBottom} />
+        <div style={{height: '35px', position: 'relative'}}>{showBottomArrow()}</div>
       </div>
-      <div style={{backgroundImage: `url('${mainImageUrl}')`, backgroundSize: 'cover', width: '650px', height: '650px', backgroundPosition: 'center'}}>
+      <div style={{backgroundImage: `url('${mainImageUrl}')`, backgroundSize: 'cover', width: '675px', height: '675px', backgroundPosition: 'center'}}>
       </div>
     </div>
 
---
+++ client/src/components/Overview/Thumbnail.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+const {useState} = React;
+
+const Thumbnail = ({currentStyle, index, selectedThumbnailIndex, setSelectedThumbnailIndex, photoObj, setMainImageUrl}) => {
+
+  const handleThumbnailClick = (e, index, photoUrl) => {
+    setMainImageUrl(photoUrl);
+    setSelectedThumbnailIndex(index);
+  }
+
+  if (index === selectedThumbnailIndex) {
+    return (
+      <div style={{margin: '15px', border: '2px solid Black'}}>
+        <div style={{backgroundImage: `url('${photoObj.url}')`, backgroundPosition: 'center', backgroundSize: 'cover', width: '100px', height: '100px', cursor: 'pointer'}} onClick={(e) => {handleThumbnailClick(e, index, photoObj.url)}}></div>
+      </div>
+    )
+  } else {
+    return (
+      <div style={{margin: '15px', padding: '2px'}}>
+        <div style={{backgroundImage: `url('${photoObj.url}')`, backgroundPosition: 'center', backgroundSize: 'cover', width: '100px', height: '100px', cursor: 'pointer'}} onClick={(e) => {handleThumbnailClick(e, index, photoObj.url)}}></div>
+      </div>
+
+    )
+  }
+}
+
+export default Thumbnail;
