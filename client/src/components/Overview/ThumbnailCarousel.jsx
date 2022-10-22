982e479297eca575d2e32a6445f593517171a499
ryan-leigh <ryanleigh25@gmail.com>
2022-10-21 17:06:34 -0700

    Fix product change scroll bug

--- client/src/components/Overview/ThumbnailCarousel.jsx
+++ client/src/components/Overview/ThumbnailCarousel.jsx
@@ -1,24 +1,28 @@
 import React from 'react';
 import Thumbnail from './Thumbnail.jsx'
-const {useState, useEffect} = React;
+const {useState, useEffect, useRef} = React;
 
 const ThumbnailCarousel = ({currentStyle, setMainImageUrl, setCarouselAtTop, setCarouselAtBottom}) => {
 
   const [selectedThumbnailIndex, setSelectedThumbnailIndex] = useState(0);
   const [carouselScrollTop, setCarouselScrollTop] = useState(0);
 
+  const carouselElem = useRef(null);
+
   const handleCarouselScroll = (e) => {
     setCarouselScrollTop(e.target.scrollTop);
+    console.log(e.target.scrollTop);
     setCarouselAtBottom(e.target.scrollHeight - e.target.scrollTop === e.target.clientHeight);
     setCarouselAtTop(e.target.scrollTop === 0);
   }
 
   useEffect(() => {
     setSelectedThumbnailIndex(0);
+    carouselElem.current.scrollTop = 0;
   }, [currentStyle]);
 
   return (
-    <div scrollTop={carouselScrollTop} className='thumbnailCarousel' style={{overflow: 'auto', maxHeight: '605px', margin: '5px'}} onScroll={(e) => {handleCarouselScroll(e)}}>
+    <div ref={carouselElem} className='thumbnailCarousel' style={{overflow: 'auto', maxHeight: '605px', margin: '5px'}} onScroll={(e) => {handleCarouselScroll(e)}}>
       {currentStyle.photos.map((photoObj, index) => (
         <Thumbnail currentStyle={currentStyle} key={index} index={index} selectedThumbnailIndex={selectedThumbnailIndex} setSelectedThumbnailIndex={setSelectedThumbnailIndex} photoObj={photoObj} setMainImageUrl={setMainImageUrl} />
       ))}
