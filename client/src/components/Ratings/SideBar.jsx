1901b5b16d8abc4963459dd9a31fd9541ef698dd
Sean McEntagart <seanmcentagart@Seans-MacBook-Pro.local>
2022-10-21 21:45:34 -0700

    added stars and progress bars for star rating count

--- client/src/components/Ratings/SideBar.jsx
+++ client/src/components/Ratings/SideBar.jsx
@@ -1,11 +1,16 @@
 import React, { useState } from 'react';
 import StarRating from './StarRating.jsx';
-
+import ProgressBar from './ProgressBar.jsx';
 const SideBar = ({ metaData, listOfReviews }) => {
   return (
-    <div>
-      <StarRating metaData={metaData} listOfReviews={listOfReviews} />
-    </div>
+    <>
+      <div>
+        <StarRating metaData={metaData} listOfReviews={listOfReviews} />
+      </div>
+      <div>
+        <ProgressBar metaData={metaData} />
+      </div>
+    </>
   );
 };
 
--- client/src/components/Ratings/StarRating.jsx
+++ client/src/components/Ratings/StarRating.jsx
@@ -7,10 +7,25 @@ const StarRating = ({ metaData, listOfReviews }) => {
     0
   );
 
+  if (metaData.recommended !== undefined) {
+    var doesRec = metaData.recommended.true;
+    var total =
+      Number(metaData.recommended.true) + Number(metaData.recommended.false);
+    var averageRec = (doesRec / total) * 100;
+  }
+
   return (
     <>
+      <div className='average-rating'>
+        {averageRating}
+        <StarRatings
+          rating={averageRating}
+          starRatedColor='gold'
+          starDimension='60px'
+        />
+      </div>
       <div>
-        <StarRatings rating={averageRating} starRatedColor='gold' />
+        {Math.round(averageRec * 10) / 10}% of reviews recommended this product!
       </div>
     </>
   );
