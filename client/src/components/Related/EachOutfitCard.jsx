ed4469fc10f62344273ceefc07cb75a8672b4e98
Roshan Patel <roshanupatel@gmail.com>
2022-10-23 21:38:20 -0700

    CSS changes to Related cards, layed out some framework for outfit cards, started to use window.localStorage

--- client/src/components/Related/EachOutfitCard.jsx
+++ client/src/components/Related/EachOutfitCard.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import Axios from 'axios';
-import CurrentInfo from '../Context.jsx';
+import { ProductIDContext } from '../Context.jsx';
 import {TOKEN} from '../../../../config.js';
 
 
 const EachOutfitCard = () => {
+  var prodIMGStyle = {
+    backgroundSize: 'cover',
+    backgroundPosition: 'center'
+  };
+
+
 
   return (
-    <div>*place holder for EachOutfitCard*
-
-    </div>
+    <li className="card">
+      <div className="card-image" style={prodIMGStyle} />
+      <div className="card-footer">
+          <div className="card-content-category">outfit category</div>
+          <div className="card-content-name">Outfit Name</div>
+          <div className="card-content-price">$100.00</div>
+      </div>
+    </li>
   );
 };
 
--- client/src/components/Related/OutfitCards.jsx
+++ client/src/components/Related/OutfitCards.jsx
@@ -2,19 +2,25 @@ import React from 'react';
 import Axios from 'axios';
 import EachOutfitCard from './EachOutfitCard.jsx';
 import {TOKEN} from '../../../../config.js';
+import AddOutfitCard from './AddOutfitCard.jsx';
 
 
 const OutfitCards = () => {
 
   //somehow access Window.localStorage to get all outfits, put them in...
-  let outfitsList = [1];
+  let outfitsList = [3, 3, 4];
 
   return (
     <div>
-      {/* <p></p>
-      {outfitsList.map((eachOutfit) =>
-        <EachOutfitCard outfit={eachOutfit} />
-      )} */}
+      <ul className="cards">
+        <AddOutfitCard />
+        <div></div>
+        {outfitsList.length !== 0 ? outfitsList.map((eachOutfit, index) => {
+          return(
+            <EachOutfitCard outfit={eachOutfit} index={index} key={index}/>
+          )}
+        ) : null}
+      </ul>
     </div>
   );
 };
