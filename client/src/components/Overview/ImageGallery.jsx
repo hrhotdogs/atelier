18c9a2f29546f04bf9e5d310abee3c390a6b734c
ryan-leigh <ryanleigh25@gmail.com>
2022-10-28 14:53:25 -0700

    Change scroll functionality and add logo bar

--- client/src/components/Overview/ImageGallery.jsx
+++ client/src/components/Overview/ImageGallery.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ThumbnailCarousel from './ThumbnailCarousel.jsx';
-const {useState, useEffect} = React;
+const {useState, useEffect, useRef} = React;
 
 
 const ImageGallery = ({currentStyle}) => {
+
+  // current range vs. currentIndex => set scrollTop (currentScrollTop +/- height of thumbnail div)
 
   // Set local state
   const [mainImageUrl, setMainImageUrl] = useState(currentStyle.photos[0].url);
@@ -11,33 +13,70 @@ const ImageGallery = ({currentStyle}) => {
   const [carouselAtBottom, setCarouselAtBottom] = useState(!(currentStyle.photos.length > 5));
   const [selectedThumbnailIndex, setSelectedThumbnailIndex] = useState(0);
 
+  const carouselContainer = useRef(null);
+  const thumbnailDivHeight = 121;
+
   const handleArrowBackClick = () => {
-    if (selectedThumbnailIndex > 0) {
-      setSelectedThumbnailIndex(selectedThumbnailIndex - 1);
-      setMainImageUrl(currentStyle.photos[selectedThumbnailIndex - 1].url);
-    }
+    setSelectedThumbnailIndex(selectedThumbnailIndex - 1);
+    setMainImageUrl(currentStyle.photos[selectedThumbnailIndex - 1].url);
+    carouselContainer.current.children[0].scrollTop = thumbnailDivHeight * (selectedThumbnailIndex - 3);
   }
 
   const handleArrowForwardClick = () => {
-    if (selectedThumbnailIndex < currentStyle.photos.length - 1) {
-      setSelectedThumbnailIndex(selectedThumbnailIndex + 1);
-      setMainImageUrl(currentStyle.photos[selectedThumbnailIndex + 1].url);
-    }
+    setSelectedThumbnailIndex(selectedThumbnailIndex + 1);
+    setMainImageUrl(currentStyle.photos[selectedThumbnailIndex + 1].url);
+    carouselContainer.current.children[0].scrollTop = thumbnailDivHeight * (selectedThumbnailIndex - 1);
   }
 
   const showTopArrow = () => {
-    if (!carouselAtTop) {
-      return  (<div style={{position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', textAlign: 'center', fontWeight: 'bold'}}><i className="arrow up"></i></div>)
+    if (selectedThumbnailIndex > 0) {
+      return  (
+        <div onClick={handleArrowBackClick} className='carouselArrowContainer'>
+          <i className="arrow up"></i>
+        </div>
+      )
     } else {
-      return (<div style={{position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', textAlign: 'center'}}></div>)
+      return (
+        <div className='carouselArrowContainer'></div>
+      )
     }
   }
 
   const showBottomArrow = () => {
-    if (!carouselAtBottom) {
-      return  (<div style={{position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', textAlign: 'center', fontWeight: 'bold'}}><i className="arrow down"></i></div>)
+    if (selectedThumbnailIndex < currentStyle.photos.length - 1) {
+      return  (
+        <div onClick={handleArrowForwardClick} className='carouselArrowContainer'>
+          <i className="arrow down"></i>
+        </div>
+      )
     } else {
-      return (<div style={{position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', textAlign: 'center'}}></div>)
+      return (
+        <div className='carouselArrowContainer'></div>
+      )
+    }
+  }
+
+  const showLeftArrow = () => {
+    if (selectedThumbnailIndex > 0) {
+      return (
+        <div className='arrowContainer arrowLeftContainer'>
+          <div onClick={handleArrowBackClick} className='arrowBox'>
+            <i className='arrow left mainBoxArrow'></i>
+          </div>
+        </div>
+      )
+    }
+  }
+
+  const showRightArrow = () => {
+    if (selectedThumbnailIndex !== currentStyle.photos.length - 1) {
+      return (
+        <div className='arrowContainer arrowRightContainer'>
+          <div onClick={handleArrowForwardClick} className='arrowBox'>
+            <i className='arrow right mainBoxArrow'></i>
+          </div>
+        </div>
+      )
     }
   }
 
@@ -51,21 +90,19 @@ const ImageGallery = ({currentStyle}) => {
   return (
     <div style={{display: 'flex', flexDirection: 'row'}} data-testid="imageGallery-1">
       <div style={{display: 'flex', flexDirection:'column', justifyContent: 'center', height: '675px'}}>
-        <div style={{height: '35px', position: 'relative'}}>{showTopArrow()}</div>
-        <ThumbnailCarousel currentStyle={currentStyle} setMainImageUrl={setMainImageUrl} setCarouselAtTop={setCarouselAtTop} setCarouselAtBottom={setCarouselAtBottom} selectedThumbnailIndex={selectedThumbnailIndex} setSelectedThumbnailIndex={setSelectedThumbnailIndex} />
-        <div style={{height: '35px', position: 'relative'}}>{showBottomArrow()}</div>
+        <div className='arrowTopBottomContainer'>
+          {showTopArrow()}
+        </div>
+        <div ref={carouselContainer}>
+          <ThumbnailCarousel currentStyle={currentStyle} setMainImageUrl={setMainImageUrl} setCarouselAtTop={setCarouselAtTop} setCarouselAtBottom={setCarouselAtBottom} selectedThumbnailIndex={selectedThumbnailIndex} setSelectedThumbnailIndex={setSelectedThumbnailIndex} />
+        </div>
+        <div className='arrowTopBottomContainer'>
+          {showBottomArrow()}
+        </div>
       </div>
       <div className='mainImage' style={{borderRadius: '2px', display: 'flex', flexDirection: 'row', backgroundImage: `url('${mainImageUrl}')`, backgroundSize: 'cover', width: '675px', height: '675px', backgroundPosition: 'center'}}>
-        <div className='arrowContainer arrowLeftContainer'>
-          <div onClick={handleArrowBackClick} className='arrowBox'>
-            <i className='arrow left mainBoxArrow'></i>
-          </div>
-        </div>
-        <div className='arrowContainer arrowRightContainer'>
-          <div onClick={handleArrowForwardClick} className='arrowBox'>
-            <i className='arrow right mainBoxArrow'></i>
-          </div>
-        </div>
+        {showLeftArrow()}
+        {showRightArrow()}
       </div>
     </div>
   )
