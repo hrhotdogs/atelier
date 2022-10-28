a3bcccd24e4855f0e1638e5cf6ab13d7b5368624
ryan-leigh <ryanleigh25@gmail.com>
2022-10-28 11:58:59 -0700

    Main image arrow style change

--- client/src/components/Overview/ImageGallery.jsx
+++ client/src/components/Overview/ImageGallery.jsx
@@ -56,10 +56,14 @@ const ImageGallery = ({currentStyle}) => {
         <div style={{height: '35px', position: 'relative'}}>{showBottomArrow()}</div>
       </div>
       <div className='mainImage' style={{borderRadius: '2px', display: 'flex', flexDirection: 'row', backgroundImage: `url('${mainImageUrl}')`, backgroundSize: 'cover', width: '675px', height: '675px', backgroundPosition: 'center'}}>
-        <div onClick={handleArrowBackClick} className='arrowLeftContainer' style={{position: 'relative', width: '50px', height: '675px'}}><i className='arrow left' style={{position: 'absolute', top: '50%', left: '40%', height: '5px', width: '5px'}}></i></div>
-        <div onClick={handleArrowForwardClick} className='arrowRightContainer' style={{position: 'relative', width: '50px', height: '675px', marginLeft: 'auto', marginRight: '0px'}}>
-          <div style={{position: 'absolute', top: '50%', left: '30%'}}>
-            <i className='arrow right' style={{height: '5px', width: '5px'}}></i>
+        <div className='arrowContainer arrowLeftContainer'>
+          <div onClick={handleArrowBackClick} className='arrowBox'>
+            <i className='arrow left mainBoxArrow'></i>
+          </div>
+        </div>
+        <div className='arrowContainer arrowRightContainer'>
+          <div onClick={handleArrowForwardClick} className='arrowBox'>
+            <i className='arrow right mainBoxArrow'></i>
           </div>
         </div>
       </div>
