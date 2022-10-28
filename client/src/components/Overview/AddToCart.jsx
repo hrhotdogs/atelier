d31823b5a1493087ec8d62cd30409dd07e5e7a31
ryan-leigh <ryanleigh25@gmail.com>
2022-10-28 10:37:25 -0700

    Small changes

--- client/src/components/Overview/AddToCart.jsx
+++ client/src/components/Overview/AddToCart.jsx
@@ -8,6 +8,7 @@ const AddToCart = ({currentStyle}) => {
   const [selectedQty, setSelectedQty] = useState(1);
   const [inStock, setInStock] = useState(true);
   const [added, setAdded] = useState(false);
+  const [showWarning, setShowWarning] = useState(false);
 
   const handleOptionChange = (e, comp) => {
     if (comp === 'size') {
@@ -20,13 +21,17 @@ const AddToCart = ({currentStyle}) => {
   }
 
   const handleAddToCart = () => {
-    const sku = Object.keys(currentStyle.skus).find(key => currentStyle.skus[key].size === selectedSize);
-    for (let i = 0; i < selectedQty; i++) {
-      Axios.post(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/cart`, {sku_id: sku}, {headers: {Authorization: TOKEN}})
-      .then(() => {
-        setAdded(true);
-      })
-      .catch(err => console.log(err));
+    if (selectedSize === 'Select size') {
+      setShowWarning(true);
+    } else {
+      const sku = Object.keys(currentStyle.skus).find(key => currentStyle.skus[key].size === selectedSize);
+      for (let i = 0; i < selectedQty; i++) {
+        Axios.post(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/cart`, {sku_id: sku}, {headers: {Authorization: TOKEN}})
+        .then(() => {
+          setAdded(true);
+        })
+        .catch(err => console.log(err));
+      }
     }
   }
 
@@ -75,14 +80,25 @@ const AddToCart = ({currentStyle}) => {
     }
   }
 
+  const showSelectSizeWarning = () => {
+    if (showWarning) {
+      return 'Please select a size!'
+    }
+  }
+
   useEffect(() => {
     setAdded(false);
     setSelectedSize('Select size');
     setSelectedQty(1);
   }, [currentStyle])
 
+  useEffect(() => {
+    setShowWarning(false);
+  }, [selectedSize])
+
   return (
     <div className='addToCartContainer'>
+      <div style={{height: '15px', color: 'red', marginBottom: '10px'}}>{showSelectSizeWarning()}</div>
       <div className='cartRow'>
         <div className='sort-menu' style={{marginLeft: '0', marginTop: '0'}}>
           <select className='sort-options' id='sizeSort' value={selectedSize} onChange={(e) => handleOptionChange(e, 'size')}>
